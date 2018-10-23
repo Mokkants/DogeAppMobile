764db5e88d24d66e67e81cf4fc65514e3ab762e5
duykamke <gusngudu@student.gu.se>
2018-10-23 20:35:25 +0200

    bug fixes

--- app/src/main/java/com/doge/dogeapp/Adapters/DogAdapter.java
+++ app/src/main/java/com/doge/dogeapp/Adapters/DogAdapter.java
@@ -1,5 +1,6 @@
 package com.doge.dogeapp.Adapters;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -11,47 +12,54 @@ import android.widget.TextView;
 import com.doge.dogeapp.Activities.R;
 import com.doge.dogeapp.Models.Dog;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogViewHolder> {
-    private Context mContext;
-    private ArrayList<Dog> mData;
+    private LayoutInflater layoutInflater;
+    private ArrayList<Dog> listDogs;
 
-    public DogAdapter(Context context, ArrayList<Dog> data)
-    {
-        this.mContext = context;
-        this.mData = data;
+    public DogAdapter(Context context) {
+        layoutInflater = LayoutInflater.from(context);
     }
 
     @NonNull
     @Override
     public DogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater mInflater = LayoutInflater.from(mContext);
-        View view = mInflater.inflate(R.layout.dog_row_item, parent, false);
-        return new DogViewHolder(view);
+        View view = layoutInflater.inflate(R.layout.dog_row_item, parent, false);
+        DogViewHolder viewHolder = new DogViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull DogViewHolder holder, int position) {
-        Dog dog = mData.get(position);
-        holder.nameDog.setText(dog.getName());
-        holder.breedDog.setText(dog.getBreed());
-        holder.socialDog.setText(dog.getShortInfo());
+        Dog dog = listDogs.get(position);
+        holder.dogName.setText(dog.getName());
+        holder.dogBreed.setText(dog.getBreed());
+        holder.dogInfo.setText(dog.getShortInfo());
     }
 
     @Override
     public int getItemCount() {
-        return mData.size();
+        return listDogs.size();
     }
 
     public class DogViewHolder extends RecyclerView.ViewHolder {
-        TextView nameDog,breedDog,socialDog,infoDog;
+        private TextView dogName;
+        private TextView dogBreed;
+        private TextView dogInfo;
+
         public DogViewHolder(@NonNull View itemView) {
             super(itemView);
-            nameDog = itemView.findViewById(R.id.dog_name);
-            breedDog = itemView.findViewById(R.id.dog_breed);
-            socialDog = itemView.findViewById(R.id.dog_social);
-            infoDog = itemView.findViewById(R.id.dog_info);
+            dogName = itemView.findViewById(R.id.dog_name);
+            dogBreed = itemView.findViewById(R.id.dog_breed);
+            dogInfo = itemView.findViewById(R.id.dog_info);
         }
+    }
+
+    public void setDogList(ArrayList<Dog> dogs) {
+        listDogs.addAll(dogs);
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/doge/dogeapp/Fragments/DogFragment.java
+++ app/src/main/java/com/doge/dogeapp/Fragments/DogFragment.java
@@ -1,16 +1,44 @@
 package com.doge.dogeapp.Fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.doge.dogeapp.Activities.R;
+import com.doge.dogeapp.Adapters.DogAdapter;
+import com.doge.dogeapp.Helpers.Settings;
+import com.doge.dogeapp.Models.Dog;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class DogFragment extends Fragment {
+    private final String URL = getString(R.string.url) + "/api/dogs";
+    private RecyclerView dogView;
+    private DogAdapter dogAdapter;
+    private ArrayList<Dog> listDog;
+
+    public DogFragment() {
+
+    }
 
     @Nullable
     @Override
@@ -18,7 +46,43 @@ public class DogFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_dog, container, false);
 
+        dogView = (RecyclerView) view.findViewById(R.id.dogList);
+        dogView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        listDog = new ArrayList<Dog>();
+        dogAdapter = new DogAdapter(getActivity());
+        dogView.setAdapter(dogAdapter);
+        fetchData();
         return view;
     }
 
+    private void fetchData() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                            Gson gson = new Gson();
+                            String dataArray = null;
+                            try {
+                                dataArray = response.getString("data");
+                            } catch (JSONException e) {
+                                Log.e(this.getClass().toString(), e.getMessage());
+                            }
+                            Dog[] allDogs = gson.fromJson(dataArray, Dog[].class);
+                            ArrayList<Dog> dogs = new ArrayList<>();
+                            for (int i = 0; i < allDogs.length; i++) {
+                                if (allDogs[i].getOwner().equals(Settings.getLoggedInUser())) {
+                                    dogs.add(allDogs[i]);
+                                }
+                            }
+                            dogAdapter.setDogList(dogs);
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        }
+                });
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        queue.add(jsonObjectRequest);
+    }
 }
