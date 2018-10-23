289f217cf2f748da0b2abb4ecad720fe290097bd
Bence Ludmány <mokkants@gmail.com>
2018-10-23 17:46:02 +0200

    Added BaseFragment and nav sets title

--- app/src/main/java/com/doge/dogeapp/Fragments/TimelineFragment.java
+++ app/src/main/java/com/doge/dogeapp/Fragments/TimelineFragment.java
@@ -3,14 +3,13 @@ package com.doge.dogeapp.Fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.doge.dogeapp.Activities.R;
 
-public class TimelineFragment extends Fragment {
+public class TimelineFragment extends BaseFragment {
 
     @Nullable
     @Override
@@ -22,4 +21,8 @@ public class TimelineFragment extends Fragment {
     }
 
 
+    @Override
+    public String getTitle() {
+        return "Timeline";
+    }
 }
